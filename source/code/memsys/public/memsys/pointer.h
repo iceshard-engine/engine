3d51ad72550666e74c2cd24b62de5c0d0e8718c4
dandielo <daniel.penkala@gmail.com>
2019-06-14 12:50:59 +0200

    Cleaning the pointer.h file. * Renamed mooned_deleter to memsys_deleter.

--- source/code/memsys/public/memsys/pointer.h
+++ source/code/memsys/public/memsys/pointer.h
@@ -7,29 +7,25 @@
 namespace memsys
 {
 
-
-/// Custom unique_pointer deleter ///
-
-
 namespace detail
 {
 
 //! \brief A unqiue_pointer deleter for objects allocated with memsys::allocator.
 template<class T>
-class mooned_deleter
+class memsys_deleter
 {
 public:
     //! \brief Empty deleter objects are not allowed.
-    mooned_deleter() noexcept = delete;
+    memsys_deleter() noexcept = delete;
 
     //! \brief Creating a deleter from an allocator.
-    mooned_deleter(memsys::allocator& alloc) noexcept;
+    memsys_deleter(memsys::allocator& alloc) noexcept;
 
     //! \brief Creating a deleter from another deleter.
-    mooned_deleter(mooned_deleter&& other) noexcept;
+    memsys_deleter(memsys_deleter&& other) noexcept;
 
     //! \brief Updating this deleter from another deleter. //#todo is this required?
-    auto operator=(mooned_deleter&& other) noexcept -> mooned_deleter&;
+    auto operator=(memsys_deleter&& other) noexcept -> memsys_deleter&;
 
     //! \brief Method required by the unique_ptr deleter concept.
     void operator()(T* object) noexcept;
@@ -42,19 +38,13 @@ private:
 } // namespace detail
 
 
-/// Custom unique_pointer types ///
-
-
 //! \brief The memsys aware uniue_pointer type.
 template<class T>
-using unique_pointer = std::unique_ptr<T, detail::mooned_deleter<T>>;
+using unique_pointer = std::unique_ptr<T, detail::memsys_deleter<T>>;
 
 //! \brief The make_unique function with an mandatory allocator object.
 template<class T, class... Args>
 auto make_unique(memsys::allocator& alloc, Args&&... args) noexcept -> unique_pointer<T>;
-
-
-/// Inline implementation ///
 
 
 #include "pointer.inl"
