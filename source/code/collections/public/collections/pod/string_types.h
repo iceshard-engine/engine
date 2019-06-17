974f216a31547d941a2efe4573972e8b1b382c10
dandielo <daniel.penkala@gmail.com>
2019-06-17 16:47:29 +0200

    Added missing string types.

---
+++ source/code/collections/public/collections/pod/string_types.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <memsys/allocator.h>
+
+namespace pod
+{
+
+template<typename CharType = char>
+struct String;
+
+template<uint32_t Size = 16, typename CharType = char>
+struct StackString;
+
+/// A simple string value.
+template<typename CharType>
+struct String
+{
+    String(mem::allocator& a);
+    String(const String& other);
+    String& operator=(const String& other);
+    ~String();
+
+    CharType& operator[](uint32_t i);
+    const CharType& operator[](uint32_t i) const;
+
+    template<uint32_t OtherCapacity>
+    String& operator=(const StackString<OtherCapacity, CharType>& other);
+    String& operator=(const CharType* other);
+
+    mem::allocator* _allocator;
+    uint32_t _size;
+    uint32_t _capacity;
+    CharType* _data;
+};
+
+/// A stack allocated string value.
+template<uint32_t Capacity, typename CharType>
+struct StackString
+{
+    StackString();
+    StackString(const char* cstring);
+    template<uint32_t OtherCapacity>
+    StackString(const StackString<OtherCapacity, CharType>& other);
+    template<uint32_t OtherCapacity>
+    StackString& operator=(const StackString<OtherCapacity, CharType>&);
+    ~StackString();
+
+    CharType& operator[](uint32_t i);
+    const CharType& operator[](uint32_t i) const;
+
+    StackString& operator=(const String<CharType>& other);
+    StackString& operator=(const CharType* cstring);
+
+    uint32_t _size;
+    CharType _data[Capacity];
+};
+
+} // namespace pod
