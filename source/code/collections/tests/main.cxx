e32386228112e507e2d80ba66ae4e0f1eb03d116
dandielo <daniel.penkala@gmail.com>
2019-06-21 12:23:43 +0200

    Added statistics to collection_test app.

--- source/code/collections/tests/main.cxx
+++ source/code/collections/tests/main.cxx
@@ -1,15 +1,25 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
 #include <core/memory.hxx>
+#include <core/string.hxx>
+#include <core/allocators/proxy_allocator.hxx>
+#include <fmt/format.h>
 
 int main(int argc, char* argv[])
 {
     int result = 0;
 
     {
-        core::memory::globals::init();
+        core::memory::globals::init_with_stats();
 
-        result = Catch::Session().run(argc, argv);
+        {
+            auto& stats = static_cast<core::memory::proxy_allocator&>(core::memory::globals::default_allocator());
+
+            result = Catch::Session().run(argc, argv);
+
+            fmt::print(stderr, "> Statistics:\n");
+            fmt::print(stderr, "- total number of allocations: {}\n", stats.allocation_count());
+        }
 
         core::memory::globals::shutdown();
     }
