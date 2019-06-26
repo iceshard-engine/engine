2d8aab0916bdb9438ef8e5e274370d0b70280489
dandielo <daniel.penkala@gmail.com>
2019-06-26 14:09:10 +0200

    Refactored the Profiler type.

--- source/code/core/public/core/debug/profiler.hxx
+++ source/code/core/public/core/debug/profiler.hxx
@@ -1,37 +1,51 @@
 #pragma once
-#include <string_view>
+#include <core/base.hxx>
 #include <chrono>
 
-namespace core
+namespace core::debug
 {
+
+
 namespace detail
 {
 
-//! Defines the maximum count of profiler contexts if the application
-//! \note This value is constant because it's for debugging purposes only.
+//! \brief Number of available profiler contexts.
+//! \todo Make this value dynamic?
 constexpr size_t profiler_context_count = 20;
 
-//! Import some std commom types
-using profiler_clock = std::chrono::steady_clock;
 
-//! Defines a single profiler (thread) contexts.
+//! \brief Declaration of a profiler context object.
+//! \remarks These objects are thread local states for tracking Profiler events.
 struct profiler_context;
 
-//! Defines a single profiler event;
+//! \brief The clock type used by the Profiler type
+using profiler_clock = std::chrono::steady_clock;
+
+
+//! \brief An single profiling event.
 struct profiler_event
 {
-    profiler_clock::time_point begin;
-    profiler_clock::time_point end;
-    int profiler_scope;
-    int profiler_color; // Used for visualization
-    char name[64];
-    std::string_view file_name;
-    int file_line;
+    //! \brief Event name.
+    std::string_view event_name;
+
+    //! \brief Start timestamp.
+    profiler_clock::time_point event_begin;
+
+    //! \brief End timestamp.
+    profiler_clock::time_point event_end;
+
+    //! \brief Scope level.
+    int event_scope;
+
+    //! \brief Custom user data.
+    const void* user_data;
 };
+
 
 using profiler_event_callback = void(*)(void*, int, const profiler_event&);
 
-}
+} // namespace detail
+
 
 //! Creates a new profiler context for the current thread.
 void initialize_profiling_context() noexcept;
@@ -54,29 +68,26 @@ void save_all_profiling_contexts() noexcept;
 //! Visits all captured events on all initialized profiling contexts.
 void visit_all_profiling_contexts(void* data, detail::profiler_event_callback callback) noexcept(false);
 
-//! Creates a scoped profiler which stars profiling on construction and ends at destruction.
+
+//! \brief A scoped Profiler type.
 class Profiler
 {
 public:
-    //! The profiler name shouldn't be a temporary.
+    //! \brief New profiler with a name.
     Profiler(std::string_view name) noexcept;
 
-    //! The profiler name shouldn't be a temporary.
-    Profiler(std::string_view name, std::string_view file, int line) noexcept;
+    //! \brief New profiler with a name and user data.
+    Profiler(std::string_view name, const void* user_data) noexcept;
 
-    //! The profiler name shouldn't be a temporary.
-    Profiler(std::string_view name, int color) noexcept;
-
-    //! The profiler name shouldn't be a temporary.
-    Profiler(std::string_view name, int color, std::string_view file, int line) noexcept;
-
+    //! \brief Publishes the profiling resutls as an event.
     ~Profiler() noexcept;
 
 private:
     const std::string_view _name;
 
-    //! The profiler context.
+    //! \brief Current profiler context.
     detail::profiler_context* _context;
 };
 
-}
+
+} // namespace core::debug
