563a6ebbc31266fb9b6ffbfddeeaf48d0d73c223
dandielo <daniel.penkala@gmail.com>
2019-08-04 11:32:33 +0200

    Added 'data_view_aligned' type.

--- source/code/collections/private/data/view.cxx
+++ source/code/collections/private/data/view.cxx
@@ -1,4 +1,5 @@
 #include <core/data/view.hxx>
+#include <core/debug/assert.hxx>
 
 namespace core
 {
@@ -7,6 +8,14 @@ namespace core
         : _data{ data }
         , _size{ size }
     {
+    }
+
+    data_view_aligned::data_view_aligned(const void* data, uint32_t size, uint32_t alignment) noexcept
+        : _data{ data }
+        , _size{ size }
+        , _align{ alignment }
+    {
+        IS_ASSERT((reinterpret_cast<std::uintptr_t>(_data) % _align) == 0, "The 'data' pointer is not properly aligned! [ expected alignment: {} ]", _align);
     }
 
 } // namespace core
--- source/code/collections/public/core/data/view.hxx
+++ source/code/collections/public/core/data/view.hxx
@@ -27,4 +27,30 @@ namespace core
     };
 
 
+
+    //! \brief This class defines a view into a chunk of raw data.
+    struct data_view_aligned final
+    {
+        //! \brief Creates a new view info the given data.
+        data_view_aligned(const void* data, uint32_t size, uint32_t alignment) noexcept;
+
+
+        //! \brief The chunk data.
+        auto data() const noexcept { return _data; }
+
+        //! \brief The chunk size.
+        auto size() const noexcept { return _size; }
+
+
+        //! \brief The data location.
+        const void* _data;
+
+        //! \brief The data size.
+        uint32_t _size;
+
+        //! \brief The data alignment.
+        uint32_t _align;
+    };
+
+
 } // namespace core
