bf1c255c7120b49cd6329ef5d2cab03de5f74dc7
dandielo <daniel.penkala@gmail.com>
2019-08-13 22:55:54 +0200

    Introduced the 'Frame' type to the engine API. + New 'Frame' interface which allows to get frame relate info. + The engine maganges two frames between updates. + The above approach allows to exchange data between frames. + If a frame is release the allocator is reset.

--- source/code/engine/public/iceshard/engine.hxx
+++ source/code/engine/public/iceshard/engine.hxx
@@ -1,9 +1,13 @@
 #pragma once
 #include <core/allocator.hxx>
+#include <core/pointer.hxx>
 #include <input_system/system.hxx>
 
 namespace iceshard
 {
+
+
+    class Frame;
 
 
     //! \brief The main class from the engine library.
@@ -15,8 +19,19 @@ namespace iceshard
         //! \brief Returns the engine revision.
         virtual auto revision() const noexcept -> uint32_t = 0;
 
+    public:
         //! \brief Returns the used input system object.
         virtual auto input_system() const noexcept -> input::InputSystem* = 0;
+
+    public:
+        //! \brief Returns the previous frame object.
+        virtual auto previous_frame() const noexcept -> const Frame& = 0;
+
+        //! \brief Returns the next frame object with all systems updated and waiting for their task completion.
+        virtual auto current_frame() noexcept -> Frame& = 0;
+
+        //! \brief Updates the engine internal state for the next frame.
+        virtual void next_frame() noexcept = 0;
     };
 
 
