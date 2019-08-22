57e511cc119df315be93782762e969a44acb3d10
dandielo <daniel.penkala@gmail.com>
2019-08-22 16:35:01 +0200

    Create an implementation for the entity manager

--- source/code/engine/public/iceshard/entity/entity_manager.hxx
+++ source/code/engine/public/iceshard/entity/entity_manager.hxx
@@ -1,6 +1,7 @@
 #pragma once
 #include <core/allocator.hxx>
 #include <core/pod/array.hxx>
+#include <core/pod/queue.hxx>
 #include <iceshard/entity/entity.hxx>
 
 namespace iceshard::entity
@@ -14,14 +15,64 @@ namespace iceshard::entity
         EntityManager(core::allocator& alloc) noexcept;
         ~EntityManager() noexcept = default;
 
+        //! \brief Total count of living entities.
+        [[nodiscard]]
+        auto count() noexcept -> uint32_t;
+
+        //! \brief Creates a entity and returns it's handle.
+        [[nodiscard]]
+        auto create() noexcept -> entity_handle_type;
+
+        //! \brief Creates a entity and returns it's handle.
+        //! \param[in] owner The owner of the entity.
+        [[nodiscard]]
+        auto create(const void* owner) noexcept -> entity_handle_type;
+
+        //! \brief Creates the requested number of entities and pushes them into the given array.
+        void create_many(
+            uint32_t count,
+            core::pod::Array<entity_handle_type>& results
+        ) noexcept;
+
+        //! \brief Creates the requested number of entities and pushes them into the given array.
+        //! \param[in] owner The owner of each created entity.
+        void create_many(
+            uint32_t count,
+            core::pod::Array<entity_handle_type>& results,
+            const void* owner
+        ) noexcept;
+
+        //! \brief Destroys the given entity.
+        void destroy(entity_handle_type entity_handle) noexcept;
+
+        //! \brief Checks if the given entity is alive.
+        [[nodiscard]]
+        bool is_alive(entity_handle_type entity_handle) noexcept;
+
+        //! \brief Counts all owned entities.
+        [[nodiscard]]
+        auto count_owned(const void* owner) noexcept -> uint32_t;
+
+        //! \brief Destroys all owned entities.
+        void destroy_owned(const void* owner) noexcept;
+
+        //! \brief Appends all entities of the given owner to the given array.
+        void get_owned(
+            const void* owner,
+            core::pod::Array<entity_handle_type>& results
+        ) noexcept;
+
     private:
         core::allocator& _allocator;
 
-        //! \brief List of handles to be reused.
-        core::pod::Array<entity_handle> _free_handles;
+        //! \brief List of free entity indices.
+        core::pod::Queue<uint32_t> _free_indices;
 
-        //! \brief List of handles in use.
-        core::pod::Array<entity_handle> _handles;
+        //! \brief List of generations in use.
+        core::pod::Array<uint16_t> _generation;
+
+        //! \brief List of owners.
+        core::pod::Array<const void*> _owner;
     };
 
 
