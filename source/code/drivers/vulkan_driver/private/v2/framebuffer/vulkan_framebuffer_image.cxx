67d9978443383e12bb7bbb5bfff08488ed911421
dandielo <daniel.penkala@gmail.com>
2020-01-31 13:59:57 +0100

    Use the right aspectMask for framebuffer color attachments.

--- source/code/drivers/vulkan_driver/private/v2/framebuffer/vulkan_framebuffer_image.cxx
+++ source/code/drivers/vulkan_driver/private/v2/framebuffer/vulkan_framebuffer_image.cxx
@@ -67,7 +67,14 @@ namespace iceshard::renderer::vulkan
         view_info.components.g = VK_COMPONENT_SWIZZLE_G;
         view_info.components.b = VK_COMPONENT_SWIZZLE_B;
         view_info.components.a = VK_COMPONENT_SWIZZLE_A;
-        view_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+        if (format == VK_FORMAT_D16_UNORM)
+        {
+            view_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+        }
+        else
+        {
+            view_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        }
         view_info.subresourceRange.baseMipLevel = 0;
         view_info.subresourceRange.levelCount = 1;
         view_info.subresourceRange.baseArrayLayer = 0;
