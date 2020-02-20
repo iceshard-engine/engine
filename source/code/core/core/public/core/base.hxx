293c7d43ca665460be4cd23c558a60dad8891fec
dandielo <daniel.penkala@gmail.com>
2020-02-20 14:44:07 +0100

    Added a base utility header.

--- source/code/core/core/public/core/base.hxx
+++ source/code/core/core/public/core/base.hxx
@@ -1,16 +1,4 @@
 #pragma once
 #include <core/build/build.hxx>
 #include <core/debug/assert.hxx>
-
-namespace core
-{
-
-    using std::addressof;
-
-    template<typename T, uint32_t Size>
-    constexpr inline auto size(T(&)[Size]) noexcept
-    {
-        return Size;
-    }
-
-} // namespace core
+#include <core/utility.hxx>
