d911347ed8fa0c1d1daa5fd1f5e8cadce0a3b48e
dandielo <daniel.penkala@gmail.com>
2020-02-28 16:47:00 +0100

    Moved ComponentBlock logic. * Removed specific block logic from the block allocator. * Created a new header for component block operations.

--- source/code/iceshard/engine/private/component/component_block_allocator.cxx
+++ source/code/iceshard/engine/private/component/component_block_allocator.cxx
@@ -48,57 +48,6 @@ namespace iceshard
         return free_block;
     }
 
-    auto ComponentBlockAllocator::alloc_block(
-        uint32_t const* alignments,
-        uint32_t const* sizes,
-        uint32_t array_count,
-        void** pointers
-    ) noexcept -> ComponentBlock*
-    {
-        ComponentBlock* free_block = alloc_block();
-
-        uint32_t const size_sum = std::accumulate(sizes, sizes + array_count, 0u);
-        uint32_t const alignment_sum = std::accumulate(alignments, alignments + array_count, 0u);
-        uint32_t const available_memory = (free_block->_block_size - sizeof(ComponentBlock)) - alignment_sum;
-
-        void* memory_pointer = free_block + 1;
-
-        // Calculate number of components able to store in this block.
-        free_block->_entity_count_max = available_memory / size_sum;
-
-        // Update pointers properly
-        struct PointerData
-        {
-            uint32_t size;
-            uint32_t alignment;
-            void** ptr;
-        };
-
-        core::memory::stack_allocator<256> pointer_alloc;
-        core::pod::Array<PointerData> pointers_data{ pointer_alloc };
-        core::pod::array::resize(pointers_data, array_count);
-
-        for (uint32_t idx = 0; idx < array_count; ++idx)
-        {
-            pointers_data[idx].size = sizes[idx];
-            pointers_data[idx].alignment = alignments[idx];
-            pointers_data[idx].ptr = &pointers[idx];
-        }
-
-        core::pod::sort(pointers_data, [](PointerData const& left, PointerData const& right) noexcept -> int
-            {
-                return left.alignment >= right.alignment;
-            });
-
-        for (PointerData& pointer_data : pointers_data)
-        {
-            *pointer_data.ptr = core::memory::utils::align_forward(memory_pointer, pointer_data.alignment);
-            memory_pointer = core::memory::utils::pointer_add(*pointer_data.ptr, pointer_data.size * free_block->_entity_count_max);
-        }
-        return free_block;
-    }
-
-
     void ComponentBlockAllocator::release_block(ComponentBlock* block) noexcept
     {
         IS_ASSERT(block->_next == nullptr, "Only last tail blocks can be released!");
