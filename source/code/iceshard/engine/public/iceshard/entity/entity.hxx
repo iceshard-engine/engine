d1fa81ffe0680621361f47025782fb3bf773ad5d
dandielo <daniel.penkala@gmail.com>
2020-02-28 16:50:18 +0100

    Fixing Entity handle. * Removed the 'Invalid' value as there is no 'Invalid' value. * Added fmt::formatter specialization.

--- source/code/iceshard/engine/public/iceshard/entity/entity.hxx
+++ source/code/iceshard/engine/public/iceshard/entity/entity.hxx
@@ -5,16 +5,7 @@ namespace iceshard
 {
 
     //! \brief A handle for a single entity.
-    enum class Entity : uint64_t
-    {
-        Invalid = 0x0
-    };
-
-    //! \brief Checks if the entity handle is valid.
-    constexpr bool valid(Entity handle) noexcept
-    {
-        return handle != Entity::Invalid;
-    }
+    enum class Entity : uint64_t { };
 
 } // namespace iceshard::entity
 
@@ -23,3 +14,19 @@ constexpr auto core::hash<iceshard::Entity>(iceshard::Entity value) noexcept
 {
     return static_cast<std::underlying_type_t<iceshard::Entity>>(value);
 }
+
+template<>
+struct fmt::formatter<iceshard::Entity>
+{
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) noexcept
+    {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    constexpr auto format(iceshard::Entity value, FormatContext& ctx) noexcept
+    {
+        return fmt::format_to(ctx.out(), "[entity:{}]", static_cast<uint64_t>(value));
+    }
+};
