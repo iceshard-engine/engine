3b849e68208e749f8436ca13c9a683cb4d8677ae
dandielo <daniel.penkala@gmail.com>
2020-05-12 01:41:19 +0200

    Working towards a small math library.

--- source/code/core/core/public/core/math/matrix.hxx
+++ source/code/core/core/public/core/math/matrix.hxx
@@ -59,6 +59,27 @@ namespace core::math
         return result;
     }
 
+    inline auto radians(f32 degrees) noexcept -> f32
+    {
+        static constexpr f32 rads_in_deg = core::math::pi / 180.f;
+        return degrees * rads_in_deg;
+    }
+
+    inline auto sqrt(f32 val) noexcept -> f32
+    {
+        return std::sqrtf(val);
+    }
+
+    inline auto sin(f32 radians) noexcept -> f32
+    {
+        return std::sinf(radians);
+    }
+
+    inline auto cos(f32 radians) noexcept -> f32
+    {
+        return std::cosf(radians);
+    }
+
     inline auto mul(mat4x4 left, mat4x4 right) noexcept -> mat4x4
     {
         mat4x4 result;
@@ -86,6 +107,12 @@ namespace core::math
         };
     }
 
+    inline auto normalize(vec3 val) noexcept -> vec3
+    {
+        f32 const invsqrt = 1.f / sqrt(val.x * val.x + val.y * val.y + val.z * val.z);
+        return { val.x * invsqrt, val.y * invsqrt, val.z * invsqrt };
+    }
+
     inline auto translate(mat4x4 left, vec3 right) noexcept -> mat4x4
     {
         left.v[3][0] += right.x;
@@ -94,12 +121,52 @@ namespace core::math
         return left;
     }
 
+    inline auto translate(vec3 displacement) noexcept -> mat4x4
+    {
+        return translate(identity<mat4x4>(), displacement);
+    }
+
     inline auto scale(mat4x4 left, vec3 right) noexcept -> mat4x4
     {
         left.v[0][0] *= right.x;
         left.v[1][1] *= right.y;
         left.v[2][2] *= right.z;
         return left;
+    }
+
+    inline auto scale(vec3 v) noexcept -> mat4x4
+    {
+        return scale(identity<mat4x4>(), v);
+    }
+
+    inline auto rotate(mat4x4 left, f32 rad, vec3 v) noexcept -> mat4x4
+    {
+        f32 const angle = rad;
+        f32 const cosv = cos(rad);
+        f32 const cosv_1 = 1.f - cosv;
+        f32 const sinv = sin(rad);
+
+        vec3 const axis = normalize(v);
+        vec3 const cos1_axis = { cosv_1 * axis.x, cosv_1 * axis.y, cosv_1 * axis.z };
+
+        mat4x4 rm = identity<mat4x4>();
+        rm.v[0][0] = cosv + cos1_axis.x * axis.x;
+        rm.v[0][1] = cos1_axis.x * axis.y + sinv * axis.z;
+        rm.v[0][2] = cos1_axis.x * axis.z - sinv * axis.y;
+
+        rm.v[1][0] = cos1_axis.x * axis.y - sinv * axis.z;
+        rm.v[1][1] = cosv + cos1_axis.y * axis.y;
+        rm.v[1][2] = cos1_axis.y * axis.z + sinv * axis.x;
+
+        rm.v[2][0] = cos1_axis.x * axis.z + sinv * axis.y;
+        rm.v[2][1] = cos1_axis.y * axis.z - sinv * axis.x;
+        rm.v[2][2] = cosv + cos1_axis.z * axis.z;
+        return mul(left, rm);
+    }
+
+    inline auto rotate(f32 rad, vec3 v) noexcept -> mat4x4
+    {
+        return rotate(identity<mat4x4>(), rad, v);
     }
 
     inline auto transpose(mat4x4 origin) noexcept -> mat4x4
