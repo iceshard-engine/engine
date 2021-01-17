fb7e47d68816ab4df9a4005453f3622b7661fcab
dandielo <dandielo@iceshard.net>
2021-01-17 14:59:25 +0100

    Updated ice::hash function signatures.

--- source/code/core/core/public/ice/hash.hxx
+++ source/code/core/core/public/ice/hash.hxx
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <ice/types.hxx>
 #include <ice/hash/murmur2.hxx>
 #include <ice/hash/murmur3.hxx>
 
@@ -7,38 +8,56 @@ namespace ice
 {
 
     template<typename T>
-    constexpr auto hash(T value) noexcept -> uint64_t;
+    constexpr auto hash(T value) noexcept -> ice::u64;
 
     template<>
-    constexpr auto hash(std::string_view value) noexcept -> uint64_t;
+    constexpr auto hash(std::string_view value) noexcept -> ice::u64;
+
+    template<>
+    constexpr auto hash(char* const value) noexcept -> ice::u64;
 
     template<typename T>
     constexpr auto hash32(T value) noexcept -> uint32_t;
 
     template<>
-    constexpr auto hash32(std::string_view value) noexcept -> uint32_t;
+    constexpr auto hash32(std::string_view value) noexcept -> ice::u32;
+
+    template<>
+    constexpr auto hash32(char const* value) noexcept -> ice::u32;
 
 
     template<typename T>
-    constexpr auto hash(T value) noexcept -> uint64_t
+    constexpr auto hash(T value) noexcept -> ice::u64
     {
-        return uint64_t{ value };
+        return static_cast<ice::u64>(value);
     }
 
     template<>
-    constexpr auto hash(std::string_view value) noexcept -> uint64_t
+    constexpr auto hash(std::string_view value) noexcept -> ice::u64
+    {
+        return ice::detail::murmur2_hash::cexpr_murmur2_x64_64(value, 0x8642DA39).h[0];
+    }
+
+    template<>
+    constexpr auto hash(char const* value) noexcept -> ice::u64
     {
         return ice::detail::murmur2_hash::cexpr_murmur2_x64_64(value, 0x8642DA39).h[0];
     }
 
     template<typename T>
-    constexpr auto hash32(T value) noexcept -> uint32_t
+    constexpr auto hash32(T value) noexcept -> ice::u32
     {
-        return uint32_t{ value };
+        return static_cast<ice::u32>(value);
     }
 
     template<>
-    constexpr auto hash32(std::string_view value) noexcept -> uint32_t
+    constexpr auto hash32(std::string_view value) noexcept -> ice::u32
+    {
+        return ice::detail::murmur3_hash::cexpr_murmur3_x86_32(value, 0x428639DA).h[0];
+    }
+
+    template<>
+    constexpr auto hash32(char const* value) noexcept -> ice::u32
     {
         return ice::detail::murmur3_hash::cexpr_murmur3_x86_32(value, 0x428639DA).h[0];
     }
