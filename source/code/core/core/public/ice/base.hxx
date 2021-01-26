c333b8e5fcdd6751f48b3941d4f637abdd2ac0b9
dandielo <dandielo@iceshard.net>
2021-01-26 22:29:30 +0100

    Added 'ice::size' function.

--- source/code/core/core/public/ice/base.hxx
+++ source/code/core/core/public/ice/base.hxx
@@ -18,4 +18,16 @@ namespace ice
     using std::exchange;
     using std::memcpy;
 
+    template<typename T>
+    constexpr auto size(T const& cont) noexcept -> decltype(cont._size)
+    {
+        return cont._size;
+    }
+
+    template<typename T, ice::u32 Size>
+    constexpr auto size(T const(&)[Size]) noexcept -> ice::u32
+    {
+        return Size;
+    }
+
 } // namespace ice
