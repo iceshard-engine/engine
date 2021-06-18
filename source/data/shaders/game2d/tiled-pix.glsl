ce0d9f3e9d324321ba07f6412429b3f21ee3d8b1
Dandielo <dandielo@iceshard.net>
2021-06-19 00:45:19 +0200

    Updated the 'tiled' shaders to use a 'material id' used to pick proper texture chunks.

--- source/data/shaders/game2d/tiled-pix.glsl
+++ source/data/shaders/game2d/tiled-pix.glsl
@@ -1,22 +1,21 @@
 #version 450
 
 layout (location = 0) in vec2 in_uv;
+layout (location = 1) flat in int in_mat;
 
 layout (location = 0) out vec4 out_color;
 
-layout(set = 1, binding = 0) uniform texture2D default_image;
-layout(set = 1, binding = 1) uniform sampler default_sampler;
-
-layout (std140, set = 1, binding = 2) uniform TilemapProps
+layout (std140, set = 1, binding = 0) uniform TilemapProps
 {
-    float width_scale;
-    float height_scale;
+    vec2 scale[4];
 } tilemap_props;
 
+layout(set = 1, binding = 1) uniform texture2D default_image[1];
+
+layout(set = 2, binding = 0) uniform sampler default_sampler;
 
 void main()
 {
-    out_color = texture(sampler2D(default_image, default_sampler), in_uv.st);
-    out_color = texture(sampler2D(default_image, default_sampler), in_uv.st * vec2(tilemap_props.width_scale, tilemap_props.height_scale));
-    // out_color = vec4(0.2, in_uv.x, in_uv.y, 1.0);
+    vec4 tex_color = texture(sampler2D(default_image[in_mat], default_sampler), in_uv.st * tilemap_props.scale[in_mat]);
+    out_color = tex_color.rgba;
 }
--- source/data/shaders/game2d/tiled-vtx.glsl
+++ source/data/shaders/game2d/tiled-vtx.glsl
@@ -3,10 +3,11 @@
 layout (location = 0) in vec2 in_pos;
 layout (location = 1) in vec2 in_uv;
 
-layout (location = 2) in vec3 in_offset;
-layout (location = 3) in float in_angle;
+layout (location = 2) in vec2 in_offset;
+layout (location = 3) in int in_matid;
 
 layout (location = 0) out vec2 out_uv;
+layout (location = 1) out int out_matid;
 
 layout (std140, set = 0, binding = 0) uniform Camera
 {
@@ -17,12 +18,12 @@ layout (std140, set = 0, binding = 0) uniform Camera
 
 void main()
 {
-    out_uv = in_uv;
+    out_matid = in_matid & 0x0000000f;
+    int matid_offset_x = (in_matid & 0x0003fff0) >> 4;
+    int matid_offset_y = (in_matid & 0xfffc0000) >> 18;
 
-//    float rx = in_pos.x * cos(in_angle) - in_pos.y * sin(in_angle);
-//    float ry = in_pos.x * sin(in_angle) + in_pos.y * cos(in_angle);
-//    vec3 pos = vec3(rx, ry, 0) + in_offset;
-    vec3 pos = vec3(in_pos, 0) + in_offset;
+    out_uv = in_uv + vec2(matid_offset_x, mattid_offset_y);
 
+    vec3 pos = vec3(in_pos, 0) + vec3(in_offset, 1);
     gl_Position = cam.clip * cam.projection * cam.view * vec4(pos, 1);
 }
