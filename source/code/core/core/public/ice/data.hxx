87a8833bc2a89acce00867e8385c65f2cd8f3f97
Dandielo <dandielo@iceshard.net>
2021-06-19 00:37:13 +0200

    Add more ice::data_view function overloads.

--- source/code/core/core/public/ice/data.hxx
+++ source/code/core/core/public/ice/data.hxx
@@ -1,5 +1,5 @@
 #pragma once
-#include <ice/base.hxx>
+#include <ice/span.hxx>
 
 namespace ice
 {
@@ -12,7 +12,7 @@ namespace ice
     };
 
     template<typename T>
-    inline auto data_view(T const* location, u32 size, u32 alignment = alignof(T)) noexcept -> Data
+    inline auto data_view(T const* location, ice::u32 size, ice::u32 alignment = alignof(T)) noexcept -> ice::Data
     {
         return Data{
             .location = location,
@@ -22,11 +22,31 @@ namespace ice
     }
 
     template<typename T>
-    inline auto data_view(T const& object, u32 alignment = alignof(T)) noexcept -> Data
+    inline auto data_view(T const& object, ice::u32 alignment = alignof(T)) noexcept -> ice::Data
     {
         return Data{
             .location = &object,
             .size = sizeof(T),
+            .alignment = alignment,
+        };
+    }
+
+    template<typename T, ice::u32 Size>
+    constexpr auto data_view(T const(&object)[Size], ice::u32 alignment = alignof(T)) noexcept -> ice::Data
+    {
+        return Data{
+            .location = &object,
+            .size = sizeof(T) * Size,
+            .alignment = alignment,
+        };
+    }
+
+    template<typename T>
+    constexpr auto data_view(ice::Span<T const> span, ice::u32 alignment = alignof(T)) noexcept -> ice::Data
+    {
+        return Data{
+            .location = span.data(),
+            .size = static_cast<ice::u32>(span.size_bytes()),
             .alignment = alignment,
         };
     }
