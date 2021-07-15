200d7b07abb8e408289b7513d03ec2a6a225b910
Dandielo <dandielo@iceshard.net>
2021-07-16 00:19:36 +0200

    Small include changes.

--- source/code/framework/framework_base/public/ice/game_camera.hxx
+++ source/code/framework/framework_base/public/ice/game_camera.hxx
@@ -33,4 +33,10 @@ namespace ice
         ice::vec2f near_far;
     };
 
+    class WorldTrait;
+
+    auto create_trait_camera(
+        ice::Allocator& alloc
+    ) noexcept -> ice::UniquePtr<ice::WorldTrait>;
+
 } // namespace ice
