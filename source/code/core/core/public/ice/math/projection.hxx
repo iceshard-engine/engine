c9290d18d9b3fb225d825b4750143bbb57788879
Dandielo <dandielo@iceshard.net>
2021-08-05 19:35:38 +0200

    Implementation consistency [improves #37]

--- source/code/core/core/public/ice/math/projection.hxx
+++ source/code/core/core/public/ice/math/projection.hxx
@@ -7,15 +7,15 @@ namespace ice::math
     inline auto perspective_fovx(
         rad field_of_view_horizontal,
         f32 aspect_ratio,
-        f32 plane_near,
-        f32 plane_far
+        f32 near_plane,
+        f32 far_plane
     ) noexcept -> mat<4, 4, f32>;
 
     inline auto perspective_fovy(
         rad field_of_view_vertical,
         f32 aspect_ratio,
-        f32 plane_near,
-        f32 plane_far
+        f32 near_plane,
+        f32 far_plane
     ) noexcept -> mat<4, 4, f32>;
 
     constexpr auto orthographic(
@@ -32,19 +32,19 @@ namespace ice::math
             f32 right,
             f32 bottom,
             f32 top,
-            f32 plane_near,
-            f32 plane_far
+            f32 near_plane,
+            f32 far_plane
         ) noexcept -> mat<4, 4, f32>
         {
             mat<4, 4, f32> result{ };
 
-            result.v[0][0] = 2.f * plane_near / (right - left);
-            result.v[1][1] = 2.f * plane_near / (top - bottom);
+            result.v[0][0] = 2.f * near_plane / (right - left);
+            result.v[1][1] = 2.f * near_plane / (top - bottom);
             result.v[2][0] = (right + left) / (right - left);
             result.v[2][1] = (top + bottom) / (top - bottom);
-            result.v[2][2] = plane_far / (plane_near - plane_far);
+            result.v[2][2] = far_plane / (near_plane - far_plane);
             result.v[2][3] = -1.f;
-            result.v[3][2] = (plane_near * plane_far) / (plane_near - plane_far);
+            result.v[3][2] = (near_plane * far_plane) / (near_plane - far_plane);
 
             // #NOTE: The below values should be used when we want to produce clip.z between -1.f .. 1.f
             //result.v[2][2] = (plane_far + plane_near) / (plane_near - plane_far);
@@ -58,35 +58,35 @@ namespace ice::math
     inline auto perspective_fovx(
         rad field_of_view_horizontal,
         f32 aspect_ratio,
-        f32 plane_near,
-        f32 plane_far
+        f32 near_plane,
+        f32 far_plane
     ) noexcept -> mat<4, 4, f32>
     {
         f32 const tan_half_fovx = ice::math::tan(rad{ field_of_view_horizontal.value * 0.5f });
 
-        f32 right = tan_half_fovx * plane_near;
+        f32 right = tan_half_fovx * near_plane;
         f32 left = -right;
         f32 top = right / aspect_ratio;
         f32 bottom = -top;
 
-        return math_detail::perspective_rhs_lrtb(left, right, bottom, top, plane_near, plane_far);
+        return math_detail::perspective_rhs_lrtb(left, right, bottom, top, near_plane, far_plane);
     }
 
     inline auto perspective_fovy(
         rad field_of_view_vertical,
         f32 aspect_ratio,
-        f32 plane_near,
-        f32 plane_far
+        f32 near_plane,
+        f32 far_plane
     ) noexcept -> mat<4, 4, f32>
     {
         f32 const tan_half_fovy = ice::math::tan(rad{ field_of_view_vertical.value * 0.5f });
 
-        f32 top = tan_half_fovy * plane_near;
+        f32 top = tan_half_fovy * near_plane;
         f32 bottom = -top;
         f32 right = top * aspect_ratio;
         f32 left = -right;
 
-        return math_detail::perspective_rhs_lrtb(left, right, bottom, top, plane_near, plane_far);
+        return math_detail::perspective_rhs_lrtb(left, right, bottom, top, near_plane, far_plane);
     }
 
     constexpr auto orthographic(
@@ -115,10 +115,10 @@ namespace ice::math
 
         result.v[0][0] = 2.f / (right - left);
         result.v[1][1] = 2.f / (top - bottom);
-        result.v[2][2] = -1.f / (far_plane - near_plane);
+        result.v[2][2] = 1.f / (near_plane - far_plane);
         result.v[3][0] = -(right + left) / (right - left);
         result.v[3][1] = -(top + bottom) / (top - bottom);
-        result.v[3][2] = -near_plane / (far_plane - near_plane);
+        result.v[3][2] = near_plane / (near_plane - far_plane);
         result.v[3][3] = 1.f;
         return result;
     }
