9e650674df2e2c78682ae4228f08387a6dca1ae8
Dandielo <dandielo@iceshard.net>
2021-11-11 23:36:57 +0100

    Added first two make_span methods. - Might need a revisit at some point. Low prio.

--- source/code/core/collections/public/ice/span.hxx
+++ source/code/core/collections/public/ice/span.hxx
@@ -14,4 +14,16 @@ namespace ice
         return static_cast<ice::u32>(span.size());
     }
 
+    template<typename T, template<typename> typename Container>
+    constexpr auto make_span(Container<T> const& cont) noexcept -> ice::Span<T const>
+    {
+        return { cont.data(), cont.size() };
+    }
+
+    template<typename T, std::size_t Size, template<typename, std::size_t> typename Container>
+    constexpr auto make_span(Container<T, Size> const& cont) noexcept -> ice::Span<T const, Size>
+    {
+        return cont;
+    }
+
 } // namespace ice
