5e6dbbb216d863c495f740bf1d6bf12208dedebf
Dandielo <dandielo@iceshard.net>
2022-10-28 00:40:10 +0200

    Fix unix builds.

--- source/code/framework/framework_base/private/traits/physics/trait_box2d.cxx
+++ source/code/framework/framework_base/private/traits/physics/trait_box2d.cxx
@@ -108,8 +108,8 @@ namespace ice
     {
         _engine = ice::addressof(engine);
 
-        _global_space = portal.allocator().create<cpSpace>();
-        cpSpaceInit(_global_space);
+        //_global_space = portal.allocator().create<cpSpace>();
+        //cpSpaceInit(_global_space);
 
         b2Vec2 gravity{ 0.f, -10.f };
         _world = portal.allocator().create<b2World>(gravity, static_cast<ice::Allocator*>(ice::addressof(portal.allocator())));
@@ -138,8 +138,8 @@ namespace ice
         ice::WorldPortal& portal
     ) noexcept
     {
-        cpSpaceDestroy(_global_space);
-        portal.allocator().deallocate(_global_space);
+        //cpSpaceDestroy(_global_space);
+        //portal.allocator().deallocate(_global_space);
 
         engine.developer_ui().unregister_widget(_devui);
         portal.allocator().destroy(_devui);
