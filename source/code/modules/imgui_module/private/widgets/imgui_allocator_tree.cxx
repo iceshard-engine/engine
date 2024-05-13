ecdc55b9a333ff1e7109b66daf07ca5ee51d0bce
Dandielo <dandielo@iceshard.net>
2024-05-13 12:27:00 +0900

    Cleanup Allocator widget ImGui commands

--- source/code/modules/imgui_module/private/widgets/imgui_allocator_tree.cxx
+++ source/code/modules/imgui_module/private/widgets/imgui_allocator_tree.cxx
@@ -2,14 +2,9 @@
 /// SPDX-License-Identifier: MIT
 
 #include "imgui_allocator_tree.hxx"
+#include <ice/devui_imgui.hxx>
 #include <ice/string/string.hxx>
 #include <imgui/imgui.h>
-
-#if ISP_WEBAPP || ISP_ANDROID
-#define IMGUI_SIZE_FMT "%lu"
-#else
-#define IMGUI_SIZE_FMT "%llu"
-#endif
 
 namespace ice::devui
 {
@@ -79,24 +74,7 @@ namespace ice::devui
                     }
                     else
                     {
-                        bool const shows_mibs = size_allocated > 1_MiB;
-                        bool const shows_kibs = size_allocated > 1_KiB;
-
-                        ice::usize const mibs = size_allocated / 1_MiB;
-                        ice::usize const kibs = ((size_allocated / 1_KiB) - (mibs * 1_KiB)).to_usize();
-
-                        if (shows_mibs)
-                        {
-                            ImGui::Text(IMGUI_SIZE_FMT " MiB " IMGUI_SIZE_FMT " KiB (" IMGUI_SIZE_FMT " bytes)", mibs.value, kibs.value, size_allocated.value);
-                        }
-                        else if (shows_kibs)
-                        {
-                            ImGui::Text(IMGUI_SIZE_FMT " KiB (" IMGUI_SIZE_FMT " bytes)", (size_allocated / 1_KiB).value, size_allocated.value);
-                        }
-                        else
-                        {
-                            ImGui::Text(IMGUI_SIZE_FMT, size_allocated.value);
-                        }
+                        ImGui::TextT("{0:P} ({0:i} bytes)", size_allocated);
                     }
                 }
 
@@ -110,24 +88,7 @@ namespace ice::devui
                     }
                     else
                     {
-                        bool const shows_mibs = size_allocated > 1_MiB;
-                        bool const shows_kibs = size_allocated > 1_KiB;
-
-                        ice::usize const mibs = size_allocated / 1_MiB;
-                        ice::usize const kibs = ((size_allocated / 1_KiB) - (mibs * 1_KiB)).to_usize();
-
-                        if (shows_mibs)
-                        {
-                            ImGui::Text(IMGUI_SIZE_FMT " MiB " IMGUI_SIZE_FMT " KiB (" IMGUI_SIZE_FMT " bytes)", mibs.value, kibs.value, size_allocated.value);
-                        }
-                        else if (shows_kibs)
-                        {
-                            ImGui::Text(IMGUI_SIZE_FMT " KiB (" IMGUI_SIZE_FMT " bytes)", (size_allocated / 1_KiB).value, size_allocated.value);
-                        }
-                        else
-                        {
-                            ImGui::Text(IMGUI_SIZE_FMT, size_allocated.value);
-                        }
+                        ImGui::TextT("{0:P} ({0:i} bytes)", size_allocated);
                     }
                 }
 
