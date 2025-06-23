be8974ca5e6567810d481205c519cbd0553b2cca
Dandielo <dandielo@iceshard.net>
2025-06-23 23:19:30 +0900

    [iceshard] Introduced 'destructor' feature for ecs storage. * Storage allows to attach a callback with userdata, and access up to two components when enties are destroyed. * Fixed a crash in QueryOperations due to filter objects references where invalidated when created from a QueryBuilder.

---
+++ source/code/iceshard/engine/public/ice/ecs/ecs_entity_storage_details.hxx
@@ -0,0 +1,18 @@
+#pragma once
+#include <ice/ecs/ecs_types.hxx>
+
+namespace ice::ecs::detail
+{
+
+    // Callback that can provide data up to 2 components
+    using EntityDestructorCallback = void(*)(void* ctx, ice::u32, ice::ecs::Entity const*, void* c0, void* c1) noexcept;
+
+    struct EntityDestructor
+    {
+        void* userdata;
+        ice::StringID identifier;
+        ice::StringID components[2]{ ice::StringID_Invalid, ice::StringID_Invalid };
+        ice::ecs::detail::EntityDestructorCallback fn;
+    };
+
+} // namespace ice::ecs::detail
