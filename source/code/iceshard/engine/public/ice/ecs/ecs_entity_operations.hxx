da9e7b9fac9fc681eb76b41342165f1e5c85f2f3
Dandielo <dandielo@iceshard.net>
2025-08-10 11:23:45 +0900

    [iceshard] Introduced new query operation 'try_entity' * Returns same data as 'for_entity' but allows to also return with nullptr when the entity couldn't be accessed.

--- source/code/iceshard/engine/public/ice/ecs/ecs_entity_operations.hxx
+++ source/code/iceshard/engine/public/ice/ecs/ecs_entity_operations.hxx
@@ -171,6 +171,9 @@ namespace ice::ecs
                         );
 
                         destroy({ entities, block->block_entity_count });
+
+                        // We want to 'unalive' entities when using this method
+                        _entities.destroy_many({ entities, block->block_entity_count });
                     }
                     block = block->next;
                 }
